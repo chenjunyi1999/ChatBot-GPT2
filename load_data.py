f0d4b895fa0128893eb1c36aaefc3dfea441a670
chenjunyi <1162369186@qq.com>
2022-01-23 20:06:09 +0800

    version 0.1

---
+++ load_data.py
@@ -0,0 +1,93 @@
+import json
+import os
+
+from tqdm import tqdm
+from transformers import GPT2Tokenizer
+
+from process_data import load_daily, load_empathetic, load_persona, load_blended
+from settings import config
+
+dataset_list = ['daily_dialog', 'empathetic_dialogues', 'persona_chat', 'blended_skill_talk']
+
+
+# 合并四个数据集
+def merge_data(tokenizer, config):
+    train_dialogues = []
+    valid_dialogues = []
+    num_train = 0
+    num_valid = 0
+    # 加载四个数据集
+    for data_name in dataset_list:
+        print(f"Processing {data_name}...")
+        if data_name == 'daily_dialog':
+            part_train_dialogues, part_valid_dialogues, part_num_train, part_num_valid = load_daily(tokenizer, config[
+                'train_frac'])
+        elif data_name == 'empathetic_dialogues':
+            part_train_dialogues, part_valid_dialogues, part_num_train, part_num_valid = load_empathetic(tokenizer,
+                                                                                                         config[
+                                                                                                             'train_frac'])
+        elif data_name == 'persona_chat':
+            part_train_dialogues, part_valid_dialogues, part_num_train, part_num_valid = load_persona(tokenizer, config[
+                'train_frac'])
+        elif data_name == 'blended_skill_talk':
+            part_train_dialogues, part_valid_dialogues, part_num_train, part_num_valid = load_blended(tokenizer, config[
+                'train_frac'])
+
+        train_dialogues += part_train_dialogues
+        valid_dialogues += part_valid_dialogues
+
+        print("#" * 50 + f"Analysis on {data_name}" + "#" * 50)
+        print(f"The number of train dialogues: {len(part_train_dialogues)}")
+        print(f"The number of valid dialogues: {len(part_valid_dialogues)}")
+        print(f"The number of train utterances: {part_num_train}")
+        print(f"The number of valid utterances: {part_num_valid}")
+
+        num_train += part_num_train
+        num_valid += part_num_valid
+    return train_dialogues, valid_dialogues, num_train, num_valid
+
+
+# 保存合并的结果
+def save_data(prefix, data_dir, dialogues, tokenizer):
+    # text
+    print(f"Saving {prefix} text file...")
+    with open(f"{data_dir}/{prefix}_utters.json", 'w') as f:
+        json.dump(dialogues, f)
+
+    # idx
+    print(f"Saving {prefix} idx file...")
+    ids = []
+    for dialogue in tqdm(dialogues):
+        dialogue_ids = []
+        for utter in dialogue:
+            tokens = tokenizer.tokenize(utter)
+            token_ids = tokenizer.convert_tokens_to_ids(tokens)
+            dialogue_ids.append(token_ids)
+        ids.append(dialogue_ids)
+
+    with open(f"{data_dir}/{prefix}_ids.json", 'w') as f:
+        json.dump(ids, f)
+
+
+if __name__ == '__main__':
+
+    data_dir = f"{config['data_dir']}"
+    if os.path.exists(data_dir):
+        print("the data has already existed!")
+    else:
+        os.makedirs(data_dir)
+        tokenizer = GPT2Tokenizer.from_pretrained(config['model_name'])
+
+        print("Loading & Merging all datasets...")
+        train_dialogues, valid_dialogues, num_train, num_valid = merge_data(tokenizer, config)
+
+        print("Saving train data...")
+        save_data(config['train_prefix'], data_dir, train_dialogues, tokenizer)
+        print("Saving validation data...")
+        save_data(config['valid_prefix'], data_dir, valid_dialogues, tokenizer)
+
+        print("#" * 50 + "Analysis on total data" + "#" * 50)
+        print(f"The number of train dialogues: {len(train_dialogues)}")
+        print(f"The number of valid dialogues: {len(valid_dialogues)}")
+        print(f"The number of train utterances: {num_train}")
+        print(f"The number of valid utterances: {num_valid}")
---
+++ mydataset.py
@@ -0,0 +1,78 @@
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
+import json
+from itertools import chain
+
+
+class CustomDataset(Dataset):
+    def __init__(self, prefix, config):
+        assert prefix == config['train_prefix'] or prefix == config['valid_prefix']
+
+        print(f"Loading {prefix}_id.json.")
+        with open(f"{config['data_dir']}/{prefix}_ids.json", 'r') as f:
+            dials = json.load(f)
+
+        self.input_ids = []        
+        self.token_type_ids = []   
+        self.labels = []           
+
+        for dial in tqdm(dials):
+            hists = []
+            for u, utter in enumerate(dial):
+                if u % 2 == 0:
+                    hists.append([config['sp1_id']] + utter)
+                else:
+                    hists.append([config['sp2_id']] + utter)
+
+            for h in range(len(hists)):
+                if hists[h][0] == config['sp2_id']:
+                    for s in range(0, h):
+                        contexts = hists[s:h + 1]
+                        if len(contexts) > config['max_turns']:
+                            num_exceeded = len(contexts) - config['max_turns']
+                            contexts = contexts[num_exceeded:]
+                        if len(contexts) < 2:
+                            break
+
+                        input_ids = [config['bos_id']] + list(chain.from_iterable(contexts)) + [config['eos_id']]
+
+                        if len(input_ids) <= config['max_len']:
+                            start_sp_id, next_sp_id = contexts[0][0], contexts[1][0]
+                            token_type_ids = [[start_sp_id] * len(ctx) if c % 2 == 0 else [next_sp_id] * len(ctx) for c, ctx in enumerate(contexts)]
+                            assert token_type_ids[-1][0] == config['sp2_id']
+                            token_type_ids = [start_sp_id] + list(chain.from_iterable(token_type_ids)) + [config['sp2_id']]
+                            assert len(input_ids) == len(token_type_ids)
+                            labels = [[-100] * len(ctx) if c < len(contexts) - 1 else [-100] + ctx[1:] for c, ctx in enumerate(contexts)]
+                            assert labels[-1][1:] == contexts[-1][1:]
+                            labels = [-100] + list(chain.from_iterable(labels)) + [config['eos_id']]
+                            assert len(input_ids) == len(labels)
+                            self.input_ids.append(input_ids)
+                            self.token_type_ids.append(token_type_ids)
+                            self.labels.append(labels)
+                            break
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.token_type_ids[idx], self.labels[idx]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+
+# Dataloader自定义数据处理函数
+class PadCollate():
+    def __init__(self, eos_id):
+        self.eos_id = eos_id
+
+    def pad_collate(self, batch):
+        input_ids, token_type_ids, labels = [], [], []
+        for idx, seqs in enumerate(batch):
+            input_ids.append(torch.LongTensor(seqs[0]))
+            token_type_ids.append(torch.LongTensor(seqs[0]))
+            labels.append(torch.LongTensor(seqs[2]))
+
+        input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=self.eos_id)
+        token_type_ids = torch.nn.utils.rnn.pad_sequence(token_type_ids, batch_first=True, padding_value=self.eos_id)
+        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=-100)
+
+        return input_ids, token_type_ids, labels
